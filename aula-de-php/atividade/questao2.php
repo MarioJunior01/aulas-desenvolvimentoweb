e6f6392f892f9e44d271b6d01c190bb0858153ee
.l <136728961+MarioJunior01@users.noreply.github.com>
2024-10-06 13:49:31 -0300

    Add files via upload

---
+++ aula-de-php/atividade/questao2.php
@@ -0,0 +1,51 @@
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+   <h1>Calcular Média </h1>
+   <form action="questao2.php" method ="GET">
+
+   <label >Nota 1</label>
+   <input type="text" name ="nota1">
+   <br>
+   <br>
+   <label> Nota2 </label>
+   <input type = "text" name = "nota2">
+   <br>
+   <br>
+   <label> Nota3 </label>
+   <input type = "text " name = "nota3">
+   <input type ="submit" value ="enviar">
+   </form>
+</body>
+</html>
+
+<?php
+if(isset($_GET["nota1"])&& isset($_GET["nota2"]) && isset($_GET["nota3"])){
+    
+    $nota1 = $_GET["nota1"];
+    $nota2 = $_GET["nota2"];
+    $nota3 = $_GET["nota3"];
+
+    $media= ($nota1+$nota2+$nota3)/3;
+    if($media<6){
+        echo "Aluno reprovado";
+    }
+    else{
+        echo "Aluno aprovado";
+    }
+    
+}
+
+
+ 
+
+
+
+
+
+?>
---
+++ aula-de-php/atividade/questao3.php
@@ -0,0 +1,36 @@
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>  Calcular </h1>
+    <form action="questao3.php" method ="GET">
+
+    <label >Numero</label>
+    <input type ="text" name = "numero">
+    <input type ="submit" value ="enviar">      
+</fomr>
+</body>
+</html>
+<?php
+
+if(isset($_GET["numero"])){
+    $numero = $_GET["numero"];
+    if($numero>0){
+        $raiz = sqrt($numero);
+        printf(" a raiz quadrada  é   %.2f",$raiz );
+    }
+    else if ($numero<0){
+        $quadrado = pow($numero,2);
+        echo "O quadrado do numero é  $quadrado";
+
+
+    }
+}
+
+
+
+
+?>
