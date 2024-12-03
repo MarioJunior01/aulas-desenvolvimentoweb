63b34f0016a289ed714ca3a404bd7d454ebca9a7
MarioJunior01 <mario.juniorifpr@gmail.com>
2024-12-03 13:34:22 -0300

    Trabalho Final php

---
+++ aula-de-php/atividade-geracaopaginas/exercicio2.php
@@ -0,0 +1,34 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="exercicio1.css">
+    <title>Document</title>
+</head>
+
+<body class="body">
+    <h1> Por favor preencha os campos abaixo</h1>
+    <form action="" method="get" class=" form">
+        <label>Nome:</label>
+        <input type="text " name="nome">
+        <br>
+        <label>Sobrenome:</label>
+        <input type="text" name="sobrenome">
+        <br>
+        <label>Idade:</label>
+        <input type="number" name="idade">
+        <br>
+        <label>Numero de Telefone:</label>
+        <input type="text" name="telefone">
+        <br>
+        <label>Cpf:</label>
+        <input type="text" name="cpf">
+        <br>
+        <label> Rg:</label>
+        <input type="text" name="rg">
+        <br>
+        <input type="submit" value="enviar">
+    </form>
+</body>
+ </html>
