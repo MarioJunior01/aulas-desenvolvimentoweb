080bcabc93a6273f0617df14cd25d59658e40bf6
MarioJunior01 <mario.juniorifpr@gmail.com>
2024-12-03 14:49:35 -0300

    Trabalho final

---
+++ trabalho-final/index.php
@@ -0,0 +1,19 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document q</title>
+    <link rel="stylesheet" href="modelo.css">
+    
+</head>
+<body class="body">
+ <nav>
+  <ul class="menu">
+    <li> <a href = "index.php"> Home </li> 
+    <li> <a href = "pagina2.php"> Cadastro </li> 
+</body>
+</html>
+
+<?php
+
+?>
