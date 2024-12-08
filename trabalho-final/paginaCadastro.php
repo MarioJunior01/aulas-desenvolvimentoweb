7672ace50ffd610c1b7439b0c2bfeef92a81cfa1
MarioJunio01 <mario.juniorifpr@gmail.com>
2024-12-08 03:08:45 -0300

    Pagina de login finalizada

---
+++ trabalho-final/paginaCadastro.php
@@ -0,0 +1,50 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de Login</title>
+    <link rel="stylesheet" href="modelo.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+
+</head>
+
+<body class="body">
+
+</body>
+<div class="menuLogin">
+    <img src="river-8286407.svg" id="imagem" width="470px" height="420px" />
+    <form action="GET" class="Login">
+        <label id="Email"> Email:</label>
+        <br>
+        <input type="text" id="CampoEmail" name="email">
+        <br>
+        <br>
+        <label id="Senha:"> Senha:</label>
+        <br>
+        <input type="password" id="CampoSenha">
+        <br>
+        <br>
+        <input type="submit" value="Log in" class="botaoLogin">
+        <br>
+        <br>
+    </form>
+    <br>
+    <br>
+    <br>
+    <br>
+    <p id="login">Tem uma conta?</p>
+    <a href="paginaLogin.php">Conecte-se</a>
+</div>
+
+</html>
+
+<?php
+
+
+?>
